697ff0269154721cad16e8c9ac1b06391654b331
zhangdongkun98 <zhangdongkun98@gmail.com>
2023-03-05 11:28:21 +0800

    sth

--- rtk_driver/rtk_driver.py
+++ rtk_driver/rtk_driver.py
@@ -10,6 +10,46 @@ import pynmea2
 from std_msgs.msg import Header
 from gps_common.msg import GPSFix
 
+
+import numpy as np
+import math
+
+
+'''
+X轴正方向为北，Y轴正方向为东。
+'''
+
+def GPStoXY(self, lat, lon, ref_lat, ref_lon):
+        # input GPS and Reference GPS in degrees
+        # output XY in meters (m) X:North Y:East
+        lat_rad = math.radians(lat)
+        lon_rad = math.radians(lon)
+        ref_lat_rad = math.radians(ref_lat)
+        ref_lon_rad = math.radians(ref_lon)
+
+        sin_lat = math.sin(lat_rad)
+        cos_lat = math.cos(lat_rad)
+        ref_sin_lat = math.sin(ref_lat_rad)
+        ref_cos_lat = math.cos(ref_lat_rad)
+
+        cos_d_lon = math.cos(lon_rad - ref_lon_rad)
+
+        arg = np.clip(ref_sin_lat * sin_lat + ref_cos_lat * cos_lat * cos_d_lon, -1.0, 1.0)
+        c = math.acos(arg)
+
+        k = 1.0
+        if abs(c) > 0:
+            k = (c / math.sin(c))
+
+        x = float(k * (ref_cos_lat * sin_lat - ref_sin_lat * cos_lat * cos_d_lon) * self.CONSTANTS_RADIUS_OF_EARTH)
+        y = float(k * cos_lat * math.sin(lon_rad - ref_lon_rad) * self.CONSTANTS_RADIUS_OF_EARTH)
+
+        return x, y
+
+
+
+
+
 class RTK:
     def __init__(self, port='/dev/ttyUSB0', ros_pub=None):
         self.serial = serial.Serial(port, 230400)
