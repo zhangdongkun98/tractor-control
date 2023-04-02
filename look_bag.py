f6982cd02492cf28ed7e5aa4f98b7eb840108e33
zhangdongkun98 <zhangdongkun98@gmail.com>
2023-04-02 11:37:58 +0800

    sth

--- look_bag.py
+++ look_bag.py
@@ -6,7 +6,8 @@ np.set_printoptions(precision=6, linewidth=65536, suppress=True, threshold=np.in
 
 import rospy
 import rosbag
-bag = rosbag.Bag(os.path.expanduser('~/dataset/agri/2023-03-12-16-43-43-rtk-loop.bag'))
+# bag = rosbag.Bag(os.path.expanduser('~/dataset/agri/2023-03-12-16-43-43-rtk-loop.bag'))
+bag = rosbag.Bag(os.path.expanduser('./results/bags/2023-03-19-18-01-52-rtk-can-circle.bag'))
 
 bag_data = bag.read_messages(topics='/rtk_data')
 
@@ -38,73 +39,23 @@ xs = np.array(xs)
 ys = np.array(ys)
 times = np.array(times)
 
-# import pdb; pdb.set_trace()
-
-
-def fit_line(x, y):
-    n = x.shape[0]
-    ones = np.ones(n)
-    # zeros = np.zeros(n)
-    A = np.stack([x, ones], axis=1)
-    b = y
-    res = np.linalg.lstsq(A, b)
-    # import pdb; pdb.set_trace()
-    dist = np.abs(res[0][0]*x -y + res[0][1]) / np.sqrt(1 + res[0][0]**2)
-    return res[0], dist
-
-
-def visualize_data(ax, x, y, x_line, y_line, dist):
-    ax.plot(x, y, 'or')
-    ax.plot(x_line, y_line, '-b')
-    # ax.set_aspect('equal', adjustable='box')
-
-    error_paths = dist
-    ratio_path = (np.where(error_paths <= 0.025, 1, 0).sum() / error_paths.shape[0]) *100
-    metric_str = f'error mean: {np.round(np.mean(error_paths), 4)}, ratio: {np.round(ratio_path, 4)} %'
-    ax.set_xlabel(metric_str, fontsize=15)
-    print(metric_str)
-
-
-
-x1, y1 = xs[1000:10000], ys[1000:10000]
-speeds1 = speeds[1000:10000]
-times1 = times[1000:10000]
-p1, dist1 = fit_line(x1, y1)   ### vertial line
-x1_line = x1
-y1_line = p1[0] * x1_line + p1[1]
-
-x2, y2 = xs[16000:-1000], ys[16000:-1000]
-speeds2 = speeds[16000:-1000]
-times2 = times[16000:-1000]
-p2, dist2 = fit_line(x2, y2)   ### vertial line
-x2_line = x2
-y2_line = p2[0] * x2_line + p2[1]
-
-
 
 
 import matplotlib.pyplot as plt
 
 fig = plt.figure(figsize=(30,16), dpi=100)
-axes = fig.subplots(2,3)
+# axes = fig.subplots(2,3)
+ax = fig.subplots(1,1)
 
-axes[0,0].plot(xs, ys, 'or')
-axes[0,0].plot(x1_line, y1_line, '-b')
-axes[0,0].plot(x2_line, y2_line, '-b')
-# axes[0,0].set_aspect('equal', adjustable='box')
+ax.plot(xs, ys, 'or')
+ax.set_aspect('equal', adjustable='box')
+bev = plt.imread('./results/bev.png')
+# from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+# im = OffsetImage(bev)
+# ab = AnnotationBbox(im, (1, 0), xycoords='axes fraction')
+# axes[0,1].add_artist(ab)
 
 
-visualize_data(axes[0,1], x1, y1, x1_line, y1_line, dist1)
-visualize_data(axes[0,2], x2, y2, x2_line, y2_line, dist2)
-
-axes[1,1].plot(speeds1, 'or')
-# axes[1,1].plot(np.hypot(np.diff(x1), np.diff(y1)) / np.diff(times1), 'og')
-axes[1,1].plot(np.hypot(np.diff(x1), np.diff(y1)) / 0.01, 'ob')
-
-axes[1,2].plot(speeds2, 'or')
-# axes[1,2].plot(np.hypot(np.diff(x2), np.diff(y2)) / np.diff(times2), 'og')
-axes[1,2].plot(np.hypot(np.diff(x2), np.diff(y2)) / 0.01, 'ob')
-
-plt.savefig('./results/rtk.png')
+plt.savefig('./results/rtk_circle.png', transparent=True)
 
 
