0a35c59b71bc431278e74915b619e89b589f7771
zhangdongkun98 <zhangdongkun98@gmail.com>
2023-04-02 16:03:59 +0800

    sth

---
+++ papers.md
@@ -0,0 +1,26 @@
+
+
+ICRA-2005: **Model Predictive Control for Vehicle Guidance in Presence of Sliding: Application to Farm Vehicles Path Tracking**. 
+
+Towards Agrobots: Identification of the Yaw Dynamics and Trajectory Tracking of an Autonomous Tractor [paper](https://arxiv.org/pdf/2104.06833.pdf)
+- MPC 精度低
+
+
+TIM-2014: **Towards Agrobots: Trajectory Control of an Autonomous Tractor Using Type-2 Fuzzy Logic Controllers**.
+
+**Design of Self-tuning Fuzzy PID Controllers for Position Tracking Control of Autonomous Agricultural Tractor**. [paper](http://119.40.116.186/resources/files/Pertanika%20PAPERS/JST%20Vol.%2027%20(1)%20Jan.%202019/14%20JST-1064-2017.pdf)
+
+
+
+**Design of a Control System for an Autonomous Vehicle Based on Adaptive-PID**. [paper](https://journals.sagepub.com/doi/pdf/10.5772/51314)
+
+Complex Trajectory Tracking Using PID Control for Autonomous Driving [paper](https://www.researchgate.net/profile/Wael-Farag/publication/335775497_Complex_Trajectory_Tracking_Using_PID_Control_for_Autonomous_Driving/links/5f0d7546299bf15bd70b198d/Complex-Trajectory-Tracking-Using-PID-Control-for-Autonomous-Driving.pdf)
+
+
+
+Navigation system for agricultural machines: Nonlinear Model Predictive path tracking
+
+Reference trajectory planning under constraints and path tracking using linear time-varying model predictive control for agricultural machines
+
+
+Robust Tube-based Decentralized Nonlinear Model Predictive Control of an Autonomous Tractor-Trailer System
--- subscriber.py
+++ subscriber.py
@@ -1,8 +1,12 @@
+import carla_utils as cu
+from carla_utils import carla, navigation
+RoadOption = navigation.local_planner.RoadOption
 
 import rospy
 import threading
 import time
 
+import numpy as np
 import matplotlib.pyplot as plt
 
 from std_msgs.msg import Header
@@ -10,6 +14,29 @@ from gps_common.msg import GPSFix
 
 
 from driver.projection import Projection
+
+
+
+from envs.env_carla import SR, FREQ
+from envs.env_agri import PseudoWaypoint
+def get_global_path():
+    start_x, start_y = 6.058451788499951, -29.40062252106145
+    end_x, end_y = 6.058451788499951, 0.0
+
+    length = np.hypot(end_x-start_x, end_y-start_y)
+    theta = np.arctan2(end_y-start_y, end_x-start_x)
+    route = []
+    current_length = 0.0
+    while True:
+        if current_length > length:
+            break
+        x = start_x + current_length * np.cos(theta)
+        y = start_y + current_length * np.sin(theta)
+        wp = PseudoWaypoint(x, y, theta)
+        route.append((wp, RoadOption.LANEFOLLOW))
+        current_length += SR
+    global_path = cu.GlobalPath(route)
+    return global_path
 
 
 
@@ -35,6 +62,8 @@ class RTK(object):
 
 
 
+
+
 if __name__ == '__main__':
     rospy.init_node('rtk_subsriber', anonymous=False)
 
@@ -42,10 +71,17 @@ if __name__ == '__main__':
     time.sleep(1.0)
 
     rate = rospy.Rate(100)
+    foreground = []
     plt.gca().set_aspect('equal')
+
+    gp = get_global_path()
+    plt.plot(gp.x, gp.y, 'ob')
+
     while not rospy.is_shutdown():
+        [i.remove() for i in foreground]
         # plt.plot(rtk.latitude, rtk.longitude, 'or')
-        plt.plot(rtk.x, rtk.y, 'or')
+        line = plt.plot(rtk.x, rtk.y, 'or')[0]
+        foreground.append(line)
         plt.pause(0.001)
         rate.sleep()
 
